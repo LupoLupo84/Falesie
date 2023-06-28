44ffff6b2c9e9ee367a184d20cd9a467c314983b
LupoLupo84 <114899175+LupoLupo84@users.noreply.github.com>
2023-06-28 16:40:18 +0200

    first release

--- app/src/main/java/com/example/falesie/firestore/Login.kt
+++ app/src/main/java/com/example/falesie/firestore/Login.kt
@@ -7,33 +7,53 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Contacts
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.History
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -54,40 +74,57 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.falesie.R
+import com.example.falesie.screen.FalesieScreen
 import com.example.falesie.ui.theme.FalesieTheme
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreen(auth: FirebaseAuth){
+fun LoginScreen(auth: FirebaseAuth) {
+
 
     val context = LocalContext.current
-
     val focusManager = LocalFocusManager.current
-
     var email by remember { mutableStateOf("") }
-
     var password by remember { mutableStateOf("") }
-
-    val isEmailValid by remember{
-        derivedStateOf{
+    val isEmailValid by remember {
+        derivedStateOf {
             Patterns.EMAIL_ADDRESS.matcher(email).matches()
         }
     }
-
     val isPasswordValid by remember {
         derivedStateOf {
             password.length > 5
         }
     }
+    var isPasswordVisible by remember { mutableStateOf(false) }
+    //val navController = rememberNavController()
 
-    var isPasswordVisible by remember { mutableStateOf( false) }
 
-
+//    Box() {
+//
+//
+//        NavHost(
+//            navController = navController,
+//            startDestination = "Login"
+//        ) {
+//            composable("Login") {
+//                LoginScreen(auth)
+//            }
+//            composable("Falesie") {
+//                FalesieScreen()
+//            }
+//
+//        }
+//
     Column(
         modifier = Modifier
             .background(color = MaterialTheme.colorScheme.background)
@@ -139,7 +176,7 @@ fun LoginScreen(auth: FirebaseAuth){
             ) {
                 OutlinedTextField(
                     value = email,
-                    onValueChange = {email = it},
+                    onValueChange = { email = it },
                     label = { Text("Indirizzo Email") },
                     placeholder = { Text("abc@domain.com") },
                     singleLine = true,
@@ -154,8 +191,8 @@ fun LoginScreen(auth: FirebaseAuth){
                     ),
                     isError = !isEmailValid,
                     trailingIcon = {
-                        if (email.isNotBlank()){
-                            IconButton(onClick = {email = ""}) {
+                        if (email.isNotBlank()) {
+                            IconButton(onClick = { email = "" }) {
                                 Icon(
                                     imageVector = Icons.Filled.Clear,
                                     contentDescription = "Clear email"
@@ -167,7 +204,7 @@ fun LoginScreen(auth: FirebaseAuth){
 
                 OutlinedTextField(
                     value = password,
-                    onValueChange = {password = it},
+                    onValueChange = { password = it },
                     label = { Text("Password") },
                     singleLine = true,
                     modifier = Modifier
@@ -181,34 +218,52 @@ fun LoginScreen(auth: FirebaseAuth){
                     ),
                     isError = !isPasswordValid,
                     trailingIcon = {
-                        IconButton(onClick = {isPasswordVisible = !isPasswordVisible}) {
+                        IconButton(onClick = {
+                            isPasswordVisible = !isPasswordVisible
+                        }) {
                             Icon(
                                 imageVector = if (isPasswordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
                                 contentDescription = "Toggle password visibility"
                             )
                         }
                     },
-                    visualTransformation = if (isPasswordVisible)  VisualTransformation.None else PasswordVisualTransformation()
+                    visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
                 )
 
-                Button(onClick = {
-                                 auth.signInWithEmailAndPassword(email, password)
-                                     .addOnCompleteListener{
-                                         if (it.isSuccessful){
-                                             Log.d("Firebase", "The user has successfully loged in")
-                                             email = ""
-                                             password = ""
-                                             Toast.makeText(context, "The user has successfully loged in", Toast.LENGTH_LONG ).show()
-                                             //TODO AVVIARE LA SCHERMATA PRINCIPALE DELL'APPLICAZIONE
-                                         }else{
-                                             Log.w("Firebase", "The user has FAILED to log in", it.exception)
-                                             email = ""
-                                             password = ""
-                                             Toast.makeText(context, "The user has FAILED to log in " +
-                                                     " ${it.exception.toString()}", Toast.LENGTH_LONG ).show()
-                                         }
-                                     }
-                },
+                Button(
+                    onClick = {
+                        auth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener {
+                                if (it.isSuccessful) {
+                                    Log.d(
+                                        "Firebase",
+                                        "The user has successfully loged in"
+                                    )
+                                    email = ""
+                                    password = ""
+                                    Toast.makeText(
+                                        context,
+                                        "The user has successfully loged in",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                    //TODO AVVIARE LA SCHERMATA PRINCIPALE DELL'APPLICAZIONE
+                                } else {
+                                    Log.w(
+                                        "Firebase",
+                                        "The user has FAILED to log in",
+                                        it.exception
+                                    )
+                                    email = ""
+                                    password = ""
+                                    Toast.makeText(
+                                        context,
+                                        "The user has FAILED to log in " +
+                                                " ${it.exception.toString()}",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }
+                            }
+                    },
                     modifier = Modifier
                         .fillMaxWidth(),
                     colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.inversePrimary),
@@ -240,22 +295,34 @@ fun LoginScreen(auth: FirebaseAuth){
         }
 
         Button(
-            onClick =  {
-                       auth.createUserWithEmailAndPassword(email, password)
-                           .addOnCompleteListener{
-                               if (it.isSuccessful){
-                                   Log.d("Firebase", "The user has successfully created")
-                                   email = ""
-                                   password = ""
-                                   Toast.makeText(context, "The user has successfully loged in", Toast.LENGTH_LONG ).show()
-                               }else{
-                                   Log.w("Firebase", "The user has FAILED to create account", it.exception)
-                                   email = ""
-                                   password = ""
-                                   Toast.makeText(context, "The user has FAILED to create account " +
-                                           " ${it.exception.toString()}", Toast.LENGTH_LONG ).show()
-                               }
-                           }
+            onClick = {
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Log.d("Firebase", "The user has successfully created")
+                            email = ""
+                            password = ""
+                            Toast.makeText(
+                                context,
+                                "The user has successfully loged in",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        } else {
+                            Log.w(
+                                "Firebase",
+                                "The user has FAILED to create account",
+                                it.exception
+                            )
+                            email = ""
+                            password = ""
+                            Toast.makeText(
+                                context,
+                                "The user has FAILED to create account " +
+                                        " ${it.exception.toString()}",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
             },
             enabled = isEmailValid && isPasswordValid,
             modifier = Modifier
@@ -273,13 +340,16 @@ fun LoginScreen(auth: FirebaseAuth){
 
     }
 
+    //}
+
+
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview(){
-    FalesieTheme{
+fun DefaultPreview() {
+    FalesieTheme {
         LoginScreen(Firebase.auth)
     }
 }
